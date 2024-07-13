df11cdd025ea6f71f8a0fa7584a8b45ffd318953
Devin <devinllu@hotmail.com>
2024-07-13 16:16:02 -0700

    refactor: modularized download script and refactored code into separate functions

--- download.py
+++ download.py
@@ -1,45 +1,57 @@
-import sys
+import email
+from email.policy import default
+
 import imaplib
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 IMAP_SERVER = 'imap.shaw.ca'
-EMAIL_ACCOUNT = "username@shaw.ca"
 EMAIL_FOLDER = "INBOX"
-OUTPUT_DIRECTORY = 'path/to/file'
 
-PASSWORD = 'PASSWORD'
+USERNAME = os.getenv('EMAIL_USERNAME')
+PASSWORD = os.getenv('EMAIL_PASSWORD')
+OUTPUT_PATH = os.getenv('LOCAL_DOWNLOAD_PATH')
 
+def login(server, username, password):
+    obj = imaplib.IMAP4_SSL(server)
+    obj.login(username, password)
 
-def process_mailbox(M):
-    """
-    Dump all emails in the folder to files in output directory.
-    """
+    return obj
 
-    rv, data = M.search(None, "ALL")
-    if rv != 'OK':
+def record_email(path, message_id, content):
+    print("Writing message "), message_id
+    f = open('%s/%s.eml' %(path, message_id), 'wb')
+    f.write(content)
+    f.close()
+
+def process_mailbox(imap_obj):
+
+    response, body = imap_obj.search(None, "ALL")
+    if response != 'OK':
         print("No messages found!")
         return
 
-    for num in data[0].split():
-        rv, data = M.fetch(num, '(RFC822)')
-        if rv != 'OK':
-            print("ERROR getting message"), num
+    for message_id in body[0].split():
+        response, data = imap_obj.fetch(message_id, '(RFC822)')
+        if response != 'OK':
+            print("ERROR getting message"), message_id
             return
-        print("Writing message "), num
-        f = open('%s/%s.eml' %(OUTPUT_DIRECTORY, num), 'wb')
-        f.write(data[0][1])
-        f.close()
+
+        record_email(OUTPUT_PATH, message_id, data[0][1])
 
 def main():
-    M = imaplib.IMAP4_SSL(IMAP_SERVER)
-    M.login(EMAIL_ACCOUNT, PASSWORD)
-    rv, data = M.select(EMAIL_FOLDER)
-    if rv == 'OK':
+    imap_obj = login(IMAP_SERVER, USERNAME, PASSWORD)
+    response, _ = imap_obj.select(EMAIL_FOLDER)
+
+    if response == 'OK':
         print("Processing mailbox: "), EMAIL_FOLDER
-        process_mailbox(M)
-        M.close()
+        process_mailbox(imap_obj)
+        imap_obj.close()
     else:
-        print("ERROR: Unable to open mailbox "), rv
-    M.logout()
+        print(f"ERROR: Unable to open mailbox\n{response}")
+    imap_obj.logout()
 
 if __name__ == "__main__":
     main()
